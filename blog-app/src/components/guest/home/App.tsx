5b82bb2b531603beda0e3ecdd36b8f200986431a
Kiwi <317484048@qq.com>
2019-02-03 18:30:40 +0100

    finish the config of antdesign

--- blog-app/src/components/guest/home/App.tsx
+++ blog-app/src/components/guest/home/App.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
-import './App.module.sass';
+import styles from './App.module.sass';
 import { Button } from 'antd';
 
 class App extends React.Component {
   public render() {
     return (
-      <Button className="App" type="primary">Button</Button>
+      <Button className={styles['App']} type="primary">Button</Button>
     );
   }
 }
